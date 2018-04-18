ce82bbfabc31dc1269fe2020d745181aa7d67f5b
StijnKlopper <33661926+StijnKlopper@users.noreply.github.com>
2018-04-18 14:57:33 +0200

    register system
    register system implemented

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-const { check, validationResult } = require('express-validator/check');
-const { matchedData, sanitize } = require('express-validator/filter');
+
+var User = require('../models/user');
+
+const {
+  check,
+  validationResult
+} = require('express-validator/check');
+const {
+  matchedData,
+  sanitize
+} = require('express-validator/filter');
 
 
 // Register
 router.get('/register', function(req, res) {
-  res.render('register');
+  res.render('register', {
+    layout: 'layoutempty'
+  });
 
 
 });
 //users
 router.get('/login', function(req, res) {
-  res.render('login');
+  res.render('login', {
+    layout: 'layoutempty'
+  });
 });
 
 
@@ -22,32 +35,48 @@ router.get('/login', function(req, res) {
 //register users
 router.post('/register', function(req, res) {
   var username = req.body.username;
-  var emailadress = req.body.email;
+  var email = req.body.email;
   var password = req.body.password;
   var repassword = req.body.repassword;
-  var township = req.body.Township;
+  var township = req.body.township;
   console.log(username);
   //validation
-  //req.check('username').exists().withMessage('must be an email');
-  req.checkBody('emailadress', 'Emailadress is required').notEmpty();
-  //req.check('emailadress').exists();
-  //req.checkBody('password', 'Password is required').notEmpty();
-  //req.checkBody('repassword', 'Password verification is required').notEmpty();
-  //req.checkBody('repassword', 'Passwords do not match').equals(req.body.password);
-  //req.checkBody('township', 'Township is required').notEmpty();
-
+  req.checkBody('username').notEmpty().withMessage('username is required');
+  req.checkBody('email').notEmpty().withMessage('email is required');
+  req.checkBody('email').isEmail().withMessage('email is not valid');
+  req.checkBody('password').notEmpty().withMessage('password is required');
+  req.checkBody('repassword').notEmpty().withMessage('password confirmation is required');
+  req.checkBody('repassword', 'Passwords do not match').equals(req.body.password);
+  //  req.checkBody('township').notEmpty().withMessage('township must be selected');
+  //console.log(township);
 
 
   var errors = req.validationErrors();
 
   if (errors) {
+    console.log("foutmelding");
     res.render('register', {
-      errors:errors
+      errors: errors
     })
 
   } else {
-    console.log('NO');
+    console.log('geen foutmelding');
+      var newUser = new User({
+        username: username,
+        email: email,
+        password: password,
+        township: 'amsterdamwest'
 
+      });
+
+      User.createUser(newUser, function(err, user){
+        if(err) throw err;
+        console.log(user);
+      });
+
+      //req.flash('succes_msg', 'You are registered and can now login');
+
+      res.redirect('/users/login');
   }
 
 
