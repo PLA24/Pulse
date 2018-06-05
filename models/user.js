257f8608482c0aa41adcd0ab97e06d08d0575e86
Ziggy <Zv3615@gmail.com>
2018-06-05 16:29:15 +0200

    able to change settings
    not yet finished but it does work

--- models/user.js
+++ models/user.js
@@ -67,17 +67,15 @@ module.exports.setPassword = function(newPassword, user, callback){
         });
     });
 }
-
-
-
-module.exports.setEmail = function(newEmail, user, callback){
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(newEmail, salt, function(err, hash) {
-            user.password = hash;
-            user.save(callback);
-        });
-    });
+module.exports.setEmail = function(newEmail, user, callback,) {
+    user.email = newEmail;
+    user.save(callback);
 }
-module.exports.setTownship = function (newTownship, user, callback) {
+module.exports.setTownship = function(newTownship, user, callback,) {
+    user.township = newTownship;
+    user.save(callback);
+}
+module.exports.setUsername = function(newUsername, user, callback,) {
+    user.username = newUsername;
     user.save(callback);
 }
